4261b1618581742547104cb37c48b8ae080bb68d
Aluno do Senac <yamazakishun29@gmail.com>
2018-03-27 22:34:18 -0300

    Exercicio 3 Escrita no arquivo

--- senac-cn-ado03/euler.c
+++ senac-cn-ado03/euler.c
@@ -1,10 +1,4 @@
-#define _POSIX_C_SOURCE 2
-#include <stdio.h>
-#include <stdlib.h>
 #include "lib.h"
-#include <math.h>
-
-
 
 int main(){
     euler();
--- senac-cn-ado03/lib.c
+++ senac-cn-ado03/lib.c
@@ -1,10 +1,14 @@
+#define _POSIX_C_SOURCE 2
 #include "lib.h"
 #define MAX 100000000000000000000.0
 
 void euler(){
+
+    FILE * data = fopen("output.data","w");
+    FILE * gnuplot = fopen("gnuplot -persistent","w");
     long double counter=10.0;
     long double primeiroDaOrdem = counter;
-    long int incremento = 1000;
+    long int incremento = 100;
     //float
     float temp1;
     //double
@@ -16,11 +20,13 @@ void euler(){
         temp1 = pow(1+(1/counter),counter);
         temp2 = pow(1+(1/counter),counter);
         temp3 = pow(1+(1/counter),counter);
-        printf("Index %Lf  Float: %f Double: %lf Long Double: %Lf \n",counter,temp1,temp2,temp3);
+        fprintf(data,"Index %Lf  Float: %f Double: %lf Long Double: %Lf \n",counter,temp1,temp2,temp3);
         if(counter == primeiroDaOrdem*10){
-            primeiroDaOrdem *= 10000;
-            incremento *= 10000;
+            primeiroDaOrdem *= 100;
+            incremento *= 100;
         }
         counter += incremento;
     }
+
+    fclose(data);
 }
