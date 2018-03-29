8cc011fc7609cefd3ebff31e8bfcce758e04543a
Aluno do Senac <yamazakishun29@gmail.com>
2018-03-29 18:14:05 -0300

    Remover printf e verificar euler para numeros grandes resultando em 1

--- senac-cn-ado03/lib.c
+++ senac-cn-ado03/lib.c
@@ -4,11 +4,11 @@
 
 void euler(){
 
-    FILE * data = fopen("output.data","w");
-    FILE * gnuplot = fopen("gnuplot -persistent","w");
-    long double counter=10.0;
-    long double primeiroDaOrdem = counter;
-    long int incremento = 100;
+    FILE * data = fopen("output.dat","w");
+    FILE * gnuplot = popen("gnuplot -persistent","w");
+    long double counter=1.0;
+    long double controleDeEscala=counter*10.0;
+    long double incremento = 1.0;
     //float
     float temp1;
     //double
@@ -16,17 +16,43 @@ void euler(){
     //long double
     long double temp3;
     //Lista_Float *lista = malloc(sizeof(Lista_Float));
+     fprintf(data,"Index\tFloat\tDouble\tLong Double:\t\n");
     while(counter <= MAX){
-        temp1 = pow(1+(1/counter),counter);
-        temp2 = pow(1+(1/counter),counter);
-        temp3 = pow(1+(1/counter),counter);
-        fprintf(data,"Index %Lf  Float: %f Double: %lf Long Double: %Lf \n",counter,temp1,temp2,temp3);
-        if(counter == primeiroDaOrdem*10){
-            primeiroDaOrdem *= 100;
-            incremento *= 100;
+        temp1 = pow(1.0+(1.0/counter),counter);
+        temp2 = pow(1.0+(1.0/counter),counter);
+        temp3 = pow(1.0+(1.0/counter),counter);
+        fprintf(data,"%Lf\t%f\t%lf\t%Lf\n",counter,temp1,temp2,temp3);
+        printf("%Lf\t%f\t%lf\t%Lf\n",counter,temp1,temp2,temp3);
+        if(counter == controleDeEscala){
+            incremento *= 10;
+            controleDeEscala *=10;
         }
         counter += incremento;
     }
 
     fclose(data);
+
+    fprintf(gnuplot,"set terminal png\n");
+    fprintf(gnuplot,"set logscale xy\n");
+    fprintf(gnuplot,"set title 'Alex Shun Yamazaki - Numero de Euler gerado com uso de float'\n");
+    fprintf(gnuplot,"set output 'euler_flt.png'\n");
+    fprintf(gnuplot,"plot 'output.dat' using 1:2\n");
+
+    fprintf(gnuplot,"set terminal png\n");
+    fprintf(gnuplot,"set logscale xy\n");
+    fprintf(gnuplot,"set title 'Alex Shun Yamazaki - Numero de Euler gerado com uso de double'\n");
+    fprintf(gnuplot,"set output 'euler_dbl.png'\n");
+    fprintf(gnuplot,"plot 'output.dat' using 1:3\n");
+
+    fprintf(gnuplot,"set terminal png\n");
+    fprintf(gnuplot,"set logscale xy\n");
+    fprintf(gnuplot,"set title 'Alex Shun Yamazaki - Numero de Euler gerado com uso de long double'\n");
+    fprintf(gnuplot,"set output 'euler_ldbl.png'\n");
+    fprintf(gnuplot,"plot 'output.dat' using 1:4\n");
+
+    
+    fflush(gnuplot);
+    fprintf(gnuplot,"exit\n");
+    pclose(gnuplot);
+    remove("output.dat");
 }
--- senac-cn-ado03/lib.h
+++ senac-cn-ado03/lib.h
@@ -4,31 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct listaFloat{
-    float elemento;
-    struct listaFloat * prox;
-}Lista_Float;
-
-typedef struct listaDouble{
-    float elemento;
-    struct listaDouble * prox;
-}Lista_Double;
-
-typedef struct listaLongDouble{
-    float elemento;
-    struct listaLongDouble * prox;
-}Lista_LongDouble;
-
 
 void euler();
-
-void inserirFloatPilha(Lista_Float** lista, float num);
-
-void inserirDoublePilha(Lista_Double**, double num);
-
-void inserirLongDoublePilha(Lista_LongDouble**,long double num);
-
-
-
 
 #endif
